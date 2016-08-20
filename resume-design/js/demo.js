90bb18273db1e52afa8a1d95d710b03c11cce635
ArcticZoo <a417803060@126.com>
2016-08-20 16:29:17 +0800

    new

---
+++ resume-design/js/demo.js
@@ -0,0 +1,15 @@
+$(document).ready(function() {
+	//生成粒子
+  $('#particles').particleground({
+    dotColor: '#e6f1f6',
+    lineColor: '#e6f1f6'
+  });
+  $('.intro').css({
+    'margin-top': -($('.intro').height() / 2)
+  });
+
+  //改变标尺坐标
+  $(document).mousemove(function(e){  //body只是元素的显示范围,document是整个文档的范围
+        $(".move-line").css("left",e.pageX);
+  });
+});
